1a609b07eb1f07dc4055e8514dd55bf56aafcfb0
Nicola Fabiani <>
2019-05-22 14:51:13 +0200

    Complete assignment

--- assignment01/src/app/app.component.ts
+++ assignment01/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment01';
+  showDetails = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showDetails = (this.showDetails) ? false : true;
+
+    let now: Date = new Date();
+    let date: Array<String> = [ String(now.getMonth() + 1), String(now.getDate()), String(now.getFullYear()) ];
+    let time: Array<String> = [ String(now.getHours()), String(now.getMinutes()) ];
+
+    for (let i of time) {
+      if (Number(i) < 10) {
+        i = "0" + 1;
+      }
+    }
+
+    this.log.push({date: date.join("/"), time: time.join(":")});
+  }
 }
